7397d1bf999b6f687d5292bac621c55750dac7b2
windfChen <q3400p@sina.cn>
2019-10-10 23:03:22 +0200

    委派模式-添加注释

--- src/main/java/com/windf/study/gof/delegate/Delegate.java
+++ src/main/java/com/windf/study/gof/delegate/Delegate.java
@@ -5,6 +5,9 @@ public class Delegate implements Business {
     public void doSomeThing(String arg1, int arg2) {
         Business business = null;
 
+        /*
+         * 判断条件最好写在配置文件，或者根据规律写
+         */
         if ("A".equals(arg1)) {
             business = new BusinessA();
         } else if ("B".equals(arg1) && arg2 == 1) {
