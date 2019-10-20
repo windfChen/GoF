36cfa79bce53246fdbf95a3ee8f8d296353afb74
windfChen <q3400p@sina.cn>
2019-10-20 15:14:38 +0800

    工厂方法注释修改

--- src/main/java/com/windf/study/gof/factory/factorymethod/FactoryController.java
+++ src/main/java/com/windf/study/gof/factory/factorymethod/FactoryController.java
@@ -1,7 +1,7 @@
 package com.windf.study.gof.factory.factorymethod;
 
 /**
- * 这里只有ABC不一样，可以使用配置文件和反射，简单工程不行，因为创建过程可能很复杂，不能统一处理
+ * 这里只有ABC不一样，可以使用配置文件和反射，简单工厂不行，因为创建过程可能很复杂，不能统一处理
  * 具体的创建过程，写到工厂方法中了
  */
 public class FactoryController {
